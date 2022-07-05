517a88f9f008ac7c9b2868a69e2918b2833e1f33
Serhii Marchenko <serge5709244@gmail.com>
2022-07-05 11:25:28 -0400

    Added mouse controls

--- Assets/Scripts/FreeCamera.cs
+++ Assets/Scripts/FreeCamera.cs
@@ -4,25 +4,54 @@ using UnityEngine;
 
 public class FreeCamera : MonoBehaviour
 {
+    [SerializeField] GameObject playerCamera;
+
+    //  Mouse
+
+    public float viewSensitivity = 500.0f;      //  Sensitivity settigs
+    public bool b_invertX, b_invertY;           //  Inversion settings
+
+    float xRotation = 0.0f;                     //  Current camera rotation
+
+
     //  Defauld input:
     //  WASD/arrows - horizontal plane
     //  E - up      Q - down
     //  Mouse - turn camera
 
-    [SerializeField] float maxSpeed = 10.0f;
-    [SerializeField] float acceleration = 5.0f;
+    [SerializeField] float maxSpeed = 15.0f;
+    [SerializeField] float acceleration = 8.0f;
 
-    [SerializeField] float brakingForce = 3f;    //  Gradual slowdown 
+    [SerializeField] float brakingForce = 6.0f;    //  Gradual slowdown 
 
     Vector3 speed;
 
     private void Start()
     {
         speed = new Vector3(0.0f, 0.0f, 0.0f);
+        Cursor.lockState = CursorLockMode.Locked;   //  Disabling cursor
+        if (!playerCamera)
+            Debug.Log("Missing player camera object for FreeCamera script");
     }
 
     void Update()
     {
+        //  Mouse
+        float mouseX = Input.GetAxis("Mouse X") * viewSensitivity * Time.deltaTime; //  Getting mouse position for player
+        float mouseY = Input.GetAxis("Mouse Y") * viewSensitivity * Time.deltaTime; //  Getting mouse position for camera
+
+        if (b_invertY)                                                  //  Camera rotation inversion
+            xRotation += mouseY;
+        else
+            xRotation -= mouseY;
+
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);                  //  Limiting camera rotation
+        playerCamera.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);  //  Rotating camera
+        transform.Rotate(Vector3.up * mouseX);                          //  Rotating player
+
+
+
+        //  Keyboard
         float x = Input.GetAxis("Sideways");  //  Getting input
         float y = Input.GetAxis("Vertical");
         float z = Input.GetAxis("Forward");
