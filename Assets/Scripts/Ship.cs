2518998ed3b8b3cc7b06a008999e349f8d7d6680
Serhii Marchenko <serge5709244@gmail.com>
2022-07-06 08:00:45 -0400

    Added random movement to ships

--- Assets/Scripts/Ship.cs
+++ Assets/Scripts/Ship.cs
@@ -13,10 +13,22 @@ public class Ship : MonoBehaviour
     void Start()
     {
         GameManager.gameManager.teams[teamID].ships.Add(this);  //  Assigns this ship to specified team
+
+        velocity = new Vector3(0.0f, 0.0f, 0.0f);
     }
 
     void Update()
     {
-        
+        Movement();
+    }
+
+
+
+    //public float speed;
+    Vector3 velocity;
+    void Movement()
+    {
+        velocity += new Vector3(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f)) * Time.deltaTime;
+        transform.position += velocity * Time.deltaTime;
     }
 }
